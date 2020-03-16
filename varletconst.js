5932af8c8933320d53291009ec5513379c35f5d8
Qairkhan <shyngys94@gmail.com>
2020-03-16 17:29:39 +0300

    easycode: let/var/const

--- varletconst.js
+++ varletconst.js
@@ -17,3 +17,10 @@ var name_age = 20; // можно
 var NameAge = 23; //не желательно (классы указываются с большой буквы)
 var nameage = 22; // не желательно
 
+var car = 'bmw';
+var car = 'merc';
+console.log(car); // merc /var можно объявить много раз , поэтому let лучше
+
+console.log(car2); //undefined
+var car2 = 'audi'; //всплытие 
+console.log(car2); //
