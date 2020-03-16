537affc8c7cff60941160bdfa08442eada99b079
Qairkhan <shyngys94@gmail.com>
2020-03-16 20:35:46 +0300

    easycode: object

---
+++ object.js
@@ -0,0 +1,18 @@
+const user = {
+    firsName: 'Wyngys',
+    age: 26,
+    isAdmin: true,
+    email: 'asd@gmail.com',
+    'user-addres': {
+        city: 'Minsk'
+    },
+    skills: ['html', 'css', 'js']
+};
+
+let value;
+
+value=user.firsName;
+
+
+console.log(value);
+console.log(user);
