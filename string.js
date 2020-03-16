e15c0a021edfc11542076ae4d00e86eef733be7d
Qairkhan <shyngys94@gmail.com>
2020-03-16 20:12:59 +0300

    easycode: string

---
+++ string.js
@@ -0,0 +1,21 @@
+const firstName = 'Wyngys';
+const lastName = 'Qairkhan';
+const age = 30;
+const str = 'Hello my name is Wyngys';
+
+let value;
+
+value = firstName + ' ' + lastName;
+
+value += 'I am ' + age;
+
+value = firstName.length; //dlina
+
+value = firstName[2]; // 3 simvol
+
+value = firstName.toUpperCase();
+
+value = str.indexOf('n') //poisk po stroke
+
+value = str.includes ('my'); //true //poisk po stroke s otvetom boolean
+console.log (value);
