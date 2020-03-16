364d8d3b9ab2d155078a8de09513a9572dfa98e2
Qairkhan <shyngys94@gmail.com>
2020-03-16 17:07:11 +0300

    easycode: tipy dannyh

---
+++ app.js
@@ -0,0 +1,22 @@
+// типы данных
+console.log(45.4, 1, 5); //Number 
+console.log(45 * "asdasd"); //Number NAN
+console.log(1 / 0); //infinity
+
+//строки - string 
+
+console.log('hello', "hello", `hello`) //три варианта написалия "" '' ``
+
+//boolean
+
+console.log(true, false);
+
+//null - значение не известно
+
+console.log(null);
+
+//undefined - переменная ничему не равна 
+
+console.log (undefined);
+
+
