56ced81280d772a276048d0273caea78718d3366
Qairkhan <shyngys94@gmail.com>
2020-03-16 17:57:03 +0300

    easycode: type preobroz2

---
+++ typePreobr.js
@@ -0,0 +1,16 @@
+let value;
+
+value = 10;
+
+console.log (value);
+console.log(typeof value);
+
+// числа в строку
+value = String (10);
+value = (10).toString(); // метод
+//булеан в строку
+
+value = String (false);
+
+//конкатинация
+value = 30 + ' ' + 30;
