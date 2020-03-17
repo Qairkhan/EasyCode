e6645118aff8afa5c7c7c26adac8d8872ca00901
Qairkhan <shyngys94@gmail.com>
2020-03-17 18:17:40 +0300

    callback2

--- callback.js
+++ callback.js
@@ -1,8 +1,45 @@
-function foo() {
-  console.log("Hello world");
+// function foo() {
+//   console.log("Hello world");
+// }
+
+// foo();
+
+// foo.field = "Wyngys";
+// console.log(foo.field);
+
+// const arr = ["Wyngys", "Altynai", "Gukhar", "Karla"];
+
+// let newArr = [];
+// for (let i = 0; i < arr.length; i++) {
+//   newArr.push(arr[i].length);
+// }
+// console.log(newArr);
+
+// let newArr2 = [];
+// for (let i = 0; i < arr.length; i++) {
+//   newArr2.push(arr[i].toUpperCase());
+// }
+// console.log(newArr2);
+
+const names = ["Wyngys", "Altynai", "Gukhar", "Karla"];
+
+function mapArray(arr, fn) {
+  const res = [];
+  for (let i = 0; i < arr.length; i++) {
+    res.push(fn(arr[i]));
+  }
+  return res;
 }
 
-foo();
+function nameLength(el) {
+  console.log(el);
+  return el;
+}
 
-foo.field = "Wyngys";
-console.log(foo.field);
+function nameToUpperCase(el) {
+  return el.toUpperCase();
+}
+
+const result = mapArray(names, nameLength);
+const result2 = mapArray(names, nameToUpperCase);
+console.log(result2);
