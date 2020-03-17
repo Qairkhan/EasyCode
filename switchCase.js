73d4becc4e05352e62abaa10b5df18c1d7c1cc57
Qairkhan <shyngys94@gmail.com>
2020-03-17 14:38:54 +0300

    switch case

--- switchCase.js
+++ switchCase.js
@@ -1,3 +1,17 @@
-b = a > 0 ? a : b + 1 ;
+// b = a > 0 ? a : b + 1 ;
 
-console.log (b);
+// console.log (b);
+
+
+let color = 'orange';
+
+switch(color) {
+    case 'orange':
+        console.log('color is orange');
+        break;
+        case 'red':
+        console.log('color is red');
+        break;
+        default:
+        console.log('default');
+}
