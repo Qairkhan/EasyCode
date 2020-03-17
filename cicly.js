e4002224f62d04d9d5be569c926d2965aa490f2c
Qairkhan <shyngys94@gmail.com>
2020-03-17 14:54:18 +0300

    for

--- cicly.js
+++ cicly.js
@@ -1,10 +1,24 @@
 // while  , do while , for , for of , for in 
 
 
-let i = 0;
+// let i = 0;
 
-while(i++ < 10) {
-    console.log(i);
+// while(i++ < 10) {
+//     console.log(i);
    
     
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+let str = 'Hello';
+let res = '';
+
+for (let i = 0; i < str.length; i++) {
+    res += str[i] + '*';
+
 }
+
+console.log(res)
