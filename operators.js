d122ab2bb4fe5d9b5c0636756a8219931ec4c2a5
Qairkhan <shyngys94@gmail.com>
2020-03-17 13:43:25 +0300

    if esle2

--- operators.js
+++ operators.js
@@ -2,22 +2,46 @@
 
 let value;
 
-value = 1 > 2;
+// value = 1 > 2;
 
-value = 2 <= 2;
+// value = 2 <= 2;
 
-value = 1 == '1' ;
-value = 1 === '1' ;
+// value = 1 == '1' ;
+// value = 1 === '1' ;
 
-value = 'a' > 'ab';
-
-
+// value = 'a' > 'ab';
 
 
-console.log(value);
 
-if (uslovie) {
-    //action
+
+// console.log(value);
+
+// if (uslovie) {
+//     //action
+// } else {
+//         //else action
+// }
+
+
+// value = 10;
+
+// if (value ===10) {
+//     console.log('value: 10');
+// } else {
+//     console.log('else');
+// }
+
+// if (value) {
+//     console.log('some action', value);
+
+// } else {
+//     console.log('else', value);
+// }
+
+value = [];
+
+if (value.length) {
+    console.log (value);
 } else {
-        //else action
+    console.log ('array is empty');
 }
