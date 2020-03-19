8bbb10e91bd53ce992a7b2dee58a98d95b049939
Qairkhan <shyngys94@gmail.com>
2020-03-19 13:31:53 +0300

    fromEntries

--- objectMetoth.js
+++ objectMetoth.js
@@ -6,6 +6,7 @@ let obj = {
 };
 
 let obj2 = {
+    name: 'Gaara',
   age: 20
 };
 
@@ -13,3 +14,9 @@ let newObj = Object.assign({}, obj, obj2);
 console.log(newObj);
 newObj = JSON.parse(JSON.stringify(obj));
 console.log(newObj.info === obj.info);
+
+
+let keys = Object.keys(obj2)
+
+let entries = Object.entries(obj2);
+let fromEntries = Object.fromEntries(['a', 'value']);
