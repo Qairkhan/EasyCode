94252a5c55f2a06d855bbdd34a509f7f2d8d8bf2
Qairkhan <shyngys94@gmail.com>
2020-03-19 16:37:48 +0300

    event2

--- event.js
+++ event.js
@@ -1,4 +1,14 @@
 const btn = document.querySelector("button");
-btn.onclick = function() {
+const link = document.querySelector("a");
+// btn.onclick = function() {
+//   console.log("click");
+// };
+
+// btn.addEventListener("click", function(e) {
+//   console.log(e);
+// });
+
+link.addEventListener("click", function(e) {
+  e.preventDefault();
   console.log("click");
-};
+});
