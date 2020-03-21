60bd8c90d7c1e13ad595da5744a48ea15d6cc89c
Qairkhan <shyngys94@gmail.com>
2020-03-21 20:43:49 +0300

    promise cb

--- promise.js
+++ promise.js
@@ -1,1 +1,7 @@
-const promise = new Promise();
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(Math.random()), 10000);
+});
+
+console.log(promise);
+
+promise.then(x => console.log(x));
