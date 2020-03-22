4316e1552e924207ade75c1c55a4b4c64fb5e0d6
Qairkhan <shyngys94@gmail.com>
2020-03-22 16:39:33 +0300

    fetch

--- fetch.js
+++ fetch.js
@@ -1,3 +1,9 @@
 fetch(("https://jsonplaceholder.typicode.com/posts")
     .then(response => console.log(response))
     .catch(err => console.log(err));
+
+function getPost(id) {
+    return fetch ('https://jsonplaceholder.typicode.com/posts').then(
+        response => response.json(),
+    );
+}
