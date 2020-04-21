4f2cb16f334de2e494a763dd5e960e550330339d
qairkhan <shyngys94@gmail.com>
2020-04-21 18:17:07 +0300

    fix

--- fetch.js
+++ fetch.js
@@ -1,6 +1,6 @@
-// fetch(("https://jsonplaceholder.typicode.com/posts")
-//     .then(response => console.log(response))
-//     .catch(err => console.log(err));
+fetch(("https://jsonplaceholder.typicode.com/posts")
+     .then(response => console.log(response))
+     .catch(err => console.log(err));
 
 function getPost(id) {
     return fetch ('https://jsonplaceholder.typicode.com/posts').then(
