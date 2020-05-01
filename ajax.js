e7357e4517ddc9d6478e905c7a0a655dfcd2a4b3
qairkhan <shyngys94@gmail.com>
2020-05-01 17:34:36 +0300

    fixed

--- ajax.js
+++ ajax.js
@@ -1,5 +1,5 @@
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
 xhr.addEventListener("load", () => {
-  console.log(xhr.responseText);
+  console.log("ajax",xhr.responseText);
 });
