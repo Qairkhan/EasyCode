d8f6bcf385e0aca44fae6d81f5244ffe4fbc8132
qairkhan <shyngys94@gmail.com>
2020-05-01 17:36:41 +0300

    fetch zaprosy

--- fetch.js
+++ fetch.js
@@ -2,8 +2,8 @@
 //     .then(response => console.log(response))
 //     .catch(err => console.log(err));
 
-// function getPost(id) {
-//     return fetch ('https://jsonplaceholder.typicode.com/posts').then(
-//         response => response.json(),
-//     );
-// }
+function getPost(id) {
+    return fetch ('https://jsonplaceholder.typicode.com/posts').then(
+        response => response.json(),
+    );
+}
